e68f1de2cb555ba7e22b935d83713d5969d38df0
Yogesh Sati <35393686+yogeshTheCaptain@users.noreply.github.com>
2025-10-31 00:39:47 +0530

    Create constants.py

---
+++ first_app/constants.py
@@ -0,0 +1,11 @@
+redirect_url = "https://localhost"
+
+base_url = "https://kite.zerodha.com"
+login_url = "https://kite.zerodha.com/api/login"
+twofa_url = "https://kite.zerodha.com/api/twofa"
+instruments_url = "https://api.kite.trade/instruments"
+
+first_app_folder = "first_app"
+zerodha_token_file = f"{first_app_folder}/zerodha_tokens.json"
+historical_data_folder = f"{first_app_folder}/historical_data"
+     
